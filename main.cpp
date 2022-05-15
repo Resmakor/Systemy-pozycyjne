a4d75e926f11428bdbe275d5650e23795196747f
Bartłomiej <52079500+Resmakor@users.noreply.github.com>
2022-05-15 19:17:15 +0200

    Add files via upload
    More systems appeared

--- main.cpp
+++ main.cpp
@@ -22,6 +22,29 @@ map <string, int> values = {
     {"F", 15}
 };
 
+string dec_to_any (int number, int base);
+int any_to_dec (string number, int base);
+
+string any_BCD_to_dec(string BCD, int BCD_type);
+string dec_to_any_BCD(int dec, int BCD_type);
+
+string dec_to_U2(int number);
+string dec_to_U1(int number);
+
+
+
+// TODO:
+// int U2_to_dec (string U2);
+// int U1_to_dec(string U1);
+
+
+int main()
+{
+    cout << dec_to_any_BCD(123, 3) << endl;
+    cout << any_BCD_to_dec(dec_to_any_BCD(123, 3), 3) << endl;
+    cout << dec_to_U2(-37) << endl;
+    cout << dec_to_U1(-113) << endl;
+} 
 
 string dec_to_any (int number, int base)
 {
@@ -66,8 +89,105 @@ int any_to_dec (string number, int base)
     return dec;
 }
 
-int main()
+string any_BCD_to_dec(string BCD, int BCD_type)
 {
-    cout << dec_to_any(123, 16) << endl;
-    cout << any_to_dec(dec_to_any(123, 16), 16) << endl;
+    string dec = "";
+    int BCD_size = BCD.size();
+    int power = 8;
+    int sum = 0;
+    for (int i = 0; i < BCD_size; i++)
+    {
+        if (BCD[i] == '1')
+        {
+            sum += power;
+        }
+
+        power /= 2;
+        if ((i + 1) % 4 == 0 and i != 0)
+        {
+            dec += to_string(sum - BCD_type);
+            sum = 0;
+            power = 8; 
+        }
+    }
+    return dec;
 }
+
+string dec_to_any_BCD(int dec, int BCD_type)
+{
+    string converted = "";
+    while(dec > 0)
+    {
+        string part = "";
+        part = dec_to_any((dec % 10) + BCD_type, 2);
+        while(part.size() % 4 != 0)
+        {
+            part = '0' + part;
+        }
+        converted = part + converted;
+        dec /= 10;
+    }
+    return converted;
+}
+
+string dec_to_U2(int number)
+{
+    if (number >= 0)
+    {
+        return '0' + dec_to_any(number, 2);       
+    }
+
+    string bin;
+    bin = '0' + dec_to_any((-1) * number, 2);
+    int LS1_index = 0;
+    int counter = bin.size() - 1;
+
+    while (bin[counter] != '1')
+    {
+        counter -= 1;
+    }
+    string U2 = "";
+    LS1_index = counter;
+    for (int i = 0; i < LS1_index; i++)
+    {
+        if (bin[i] == '0')
+        {
+            U2 += "1";
+        }
+        else
+        {
+            U2 += "0";
+        }
+    }
+
+    for (int i = LS1_index; i < bin.size(); i++)
+    {
+        U2 += bin[i];
+    }
+    return U2;
+}
+
+string dec_to_U1(int number)
+{
+    if (number >= 0)
+    {
+        return '0' + dec_to_any(number, 2);       
+    }
+
+    string bin = '0' + dec_to_any((-1) * number, 2);
+    string U1 = "";
+
+    for (int i = 0; i < bin.size(); i++)
+    {
+        if (bin[i] == '0')
+        {
+            U1 += "1";
+        }
+
+        else
+        {
+            U1 += "0";
+        }
+    }
+    return U1;
+}   
